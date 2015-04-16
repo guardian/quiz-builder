c79fc70b4445b7cf8eb45c6f93480796ac36a3f5
Robert Berry <robert.berry@theguardian.com>
2015-04-16 16:38:35 +0100

    Some initial styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps');
 
+gulp.task('default', function () {
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
+
 gulp.task('sass', function () {
     gulp.src('./scss/*.scss')
         .pipe(sass())
