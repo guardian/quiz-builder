63ce2b60f022fc6884bf61a60cabf46c6ad01a53
Robert Berry <robert.berry@theguardian.com>
2015-04-16 15:52:51 +0100

    Add ability to create new questions

--- lib/components.jsx
+++ lib/components.jsx
@@ -9,17 +9,30 @@ class Answer extends React.Component {
 }
 
 class Question extends React.Component {
+    constructor(props) {
+        super(props);
+
+        console.log("Creating question", props);
+
+        this.state = {
+            isExpanded: false
+        };
+    }
+    
     render() {
-        const classes = classNames({
+        const classes = classnames({
             'quiz-builder__question': true,
-            'quiz-builder__question--expanded': this.props.isExpanded
+            'quiz-builder__question--expanded': this.state.isExpanded
         });
         const question = this.props.question;
+        let answersData = question.get('multiChoiceAnswers');
         let answers;
 
-        if (this.props.isExpanded && question.multiChoiceAnswers.length > 0) {
+        console.log("Hi in a question");
+
+        if (this.props.isExpanded && answersData.size > 0) {
             answers = <div className="quiz-builder__answers">
-                {question.multiChoiceAnswers.map(answer => new Answer(answer))}
+                {answersData.map(answer => <Answer answer={answer} />).toJS()}
             </div>
         }
             
@@ -41,14 +54,38 @@ export class QuizBuilder extends React.Component {
         });
     }
 
+    updateState(f) {
+        const nextState = f(this.state);
+
+        if (nextState !== this.state) {
+            this.state = nextState;
+            this.forceUpdate();
+        }
+    }
+
     addQuestion() {
-        
+        this.updateState(state => state.update(
+            'questions', 
+            questions => questions.push(Immutable.fromJS({
+                question: "",
+                more: "",
+                multiChoiceAnswers: []
+            }))
+        ));
     }
     
     render() {
+        console.log("hi", this.state.toJS());
+
+        const questions = this.state.get('questions')
+              .map(question => <Question question={question} />)
+            .toJS()
+
+        console.log("Questions", questions);
+        
         return <div className="quiz-builder">
             <div className="quiz-builder__questions">
-                {this.state.get("questions").map(question => new Question(question))}
+                {questions}
             </div>
 
             <button className="quiz-builder__new-question" onClick={this.addQuestion.bind(this)}>New question</button>
