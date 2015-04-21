8964cbeef5e4a5e3bc5e730980dbbcacbb544900
Robert Berry <robert.berry@theguardian.com>
2015-04-21 15:25:29 +0100

    ugly inputs

--- lib/ResultGroups.jsx
+++ lib/ResultGroups.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import ElasticTextArea from './ElasticTextArea';
+import {close} from './svgs.jsx!';
 import map from 'lodash-node/modern/collection/map';
 import sortBy from 'lodash-node/modern/collection/sortBy';
 
+class Range extends React.Component {
+
+    render() {
+        return <div className="range-input">
+                <input type="text" className="range-input__input range-input__input--left" value={this.props.minValue} />&ndash;<input type="text" className="range-input__input range-input__input--right" value={this.props.maxValue} />
+            </div>;
+    }
+}
+
 class ResultGroup extends React.Component {
+    removeGroup() {
+        console.log("remove group");
+    }
+    
     render() {
         return <div className="quiz-builder__result-group">
+            <Range minValue={this.props.group.get('minScore')} maxValue={this.props.group.get('maxScore')} />
             <ElasticTextArea className="quiz-builder__answer-text" value={this.props.group.get('title')} placeholder="Enter message text here ..." />
             <ElasticTextArea className="quiz-builder__answer-text" value={this.props.group.get('share')} placeholder="Enter share text here ..." />
+            <button className="quiz-builder__answer-close" onClick={this.removeGroup.bind(this)}>{close(16)}</button>
         </div>;
     }
 }
 
 export default class ResultGroups extends React.Component {
     onAddGroup() {
-        this.props.addGroup();
+        console.log("add group");
     }
     
     render() {
