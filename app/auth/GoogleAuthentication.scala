66d4965e991d34ff8275d8a6e7c37e11ac637571
Robert Berry <robert.berry@theguardian.com>
2015-05-06 15:57:02 +0100

    Eeh...

--- app/auth/GoogleAuthentication.scala
+++ app/auth/GoogleAuthentication.scala
@@ -6,25 +6,17 @@ import org.joda.time.Duration
 import play.api.mvc.Call
 import controllers.routes
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-
 object GoogleAuthentication {
   val AntiForgeryKey = "antiForgeryToken"
 
-  val googleAuthConfig = Await.result(for {
-    clientId <- config.clientId
-    secret <- config.clientSecret
-    redirectHost <- config.redirectHost
-  } yield GoogleAuthConfig(
-    clientId.get,
-    secret.get,
-    redirectHost.get,
+  val googleAuthConfig = GoogleAuthConfig(
+    config.clientId.get,
+    config.clientSecret.get,
+    config.redirectHost.get,
     Some("guardian.co.uk"),
     Some(Duration.standardHours(1)),
     enforceValidity = false
-  ), 10.seconds)
+  )
 }
 
 trait AuthActions extends Actions {
--- app/conf/Config.scala
+++ app/conf/Config.scala
@@ -4,14 +4,17 @@ import data.ConfigTable
 import play.api.Play.current
 import play.api.Play.{configuration => config}
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 object Config {
   object dynamodb {
     val tablePrefix = config.getString("dynamo_db.table_prefix")
   }
 
   object googleauth {
-    lazy val clientId = ConfigTable.get("googleauth.client_id")
-    lazy val clientSecret = ConfigTable.get("googleauth.client_secret")
-    lazy val redirectHost = ConfigTable.get("googleauth.redirect_host")
+    lazy val clientId = Await.result(ConfigTable.get("googleauth.client_id"), 10.seconds)
+    lazy val clientSecret = Await.result(ConfigTable.get("googleauth.client_secret"), 10.seconds)
+    lazy val redirectHost = Await.result(ConfigTable.get("googleauth.redirect_host"), 10.seconds)
   }
 }
