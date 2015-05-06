dbbbdd52a621092102d9f0674e050f2d7973e756
Robert Berry <robert.berry@theguardian.com>
2015-05-06 16:21:45 +0100

    Unberry it

--- app/data/ConfigTable.scala
+++ app/data/ConfigTable.scala
@@ -1,24 +1,19 @@
 package data
 
-import awswrappers.dynamodb._
 import com.amazonaws.services.dynamodbv2.model.{AttributeValue, GetItemRequest}
 import conf.Config
 import collection.JavaConverters._
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object ConfigTable {
   val TableName = Config.dynamodb.tablePrefix.getOrElse("") + "Config"
 
-  def get(key: String): Future[Option[String]] = {
-    dynamoDbClient.getItemFuture(new GetItemRequest()
+  def get(key: String): Option[String] = {
+    Option(dynamoDbClient.getItem(new GetItemRequest()
       .withTableName(TableName)
       .withKey(Map(
         "key" -> new AttributeValue().withS(key)
       ).asJava)
       .withAttributesToGet("value")
-    ) map { result =>
-      Option(result.getItem).flatMap(_.asScala.get("value").map(_.getS))
-    }
+    ).getItem).flatMap(_.asScala.get("value").map(_.getS))
   }
 }
