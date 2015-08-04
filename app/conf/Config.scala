419e4944fa595c219a6b352991f81bfffb4b560c
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-08-04 10:28:17 +0100

    Get the awsKeys from the config if they're available

--- app/conf/Config.scala
+++ app/conf/Config.scala
@@ -3,6 +3,7 @@ package conf
 import data.ConfigTable
 import play.api.Play.current
 import play.api.Play.{configuration => config}
+import com.amazonaws.auth.BasicAWSCredentials
 
 object Config {
   object dynamodb {
@@ -20,5 +21,21 @@ object Config {
     lazy val bucket = ConfigTable.get("deployment.bucket")
     lazy val accessKeyId = ConfigTable.get("deployment.access_key_id")
     lazy val secretAccessKey = ConfigTable.get("deployment.secret_access_key")
+  }
+
+  object awsKeys {
+    case class AWSCredentials(accessKey:String)(val secretKey:String) {
+      lazy val awsApiCreds = new BasicAWSCredentials(accessKey, secretKey)
+    }
+    object AWSCredentials {
+      def apply(accessKey: Option[String], secretKey: Option[String]): Option[AWSCredentials] = for {
+        ak <- accessKey
+        sk <- secretKey
+      } yield AWSCredentials(ak)(sk)
+    }
+
+    val accessKey: Option[String] = config.getString("AWS_ACCESS_KEY")
+    val secretKey: Option[String] = config.getString("AWS_SECRET_KEY")
+    val creds = AWSCredentials(accessKey, secretKey)
   }
 }
